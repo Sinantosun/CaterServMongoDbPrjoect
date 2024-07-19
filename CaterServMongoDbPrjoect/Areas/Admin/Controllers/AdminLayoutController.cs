c3c270287df0bc12fa064808085c0a2de31061bf
Sinantosun <sinan_tosun4141@hotmail.com>
2024-07-19 19:11:07 +0300

    ---

---
+++ CaterServMongoDbPrjoect/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaterServMongoDbPrjoect.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CaterServMongoDbPrjoect/ViewComponents/Admin/_AdminScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaterServMongoDbPrjoect.ViewComponents.Admin
+{
+    public class _AdminScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
